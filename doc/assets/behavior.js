edc2df343a287173eba16082ab3d8858440c613e
bruno campos barone <brunocbarone1@yahoo.com.br>
2018-03-28 14:04:14 -0300

    inserindo HTMLs

--- doc/assets/behavior.js
+++ doc/assets/behavior.js
@@ -86,8 +86,7 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
                 "pageTitle": "code\\ConfigurarNavegadorSelenium\\Navegador_java",
                 "title": "Navegador_java"
               },
-              "depth": 3,
-              "outline": []
+              "depth": 3
             }
           ]
         }, {
