82c3e5be5df00426e735e4ff8973f571b7547b16
bruno campos barone <brunocbarone1@yahoo.com.br>
2018-03-28 09:55:20 -0300

    sahsauh

---
+++ doc/suaclasse_java.java
@@ -0,0 +1,16 @@
+/*
+   <h1>TITULO DA CLASSE</h1>
+		Descrição da classe e sua funcionalidade
+		
+	  Autor: XXXXX xxxx
+	*/
+	 
+	 
+	 public class NOMEDACLASSE {
+
+
+	public static void main(String[] args) {
+
+	}
+
+}	
