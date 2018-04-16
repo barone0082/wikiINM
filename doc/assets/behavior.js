cd028dd22cc3df11f00ef2b0166e9d673dcdae3d
barone <brunocbarone1@yahoo.com.br>
2018-04-16 10:45:18 -0300

    inserindo HTMLs

--- doc/assets/behavior.js
+++ doc/assets/behavior.js
@@ -149,6 +149,36 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
         }, {
           "type": "folder",
           "data": {
+            "path": "code/RandomNumberGenerator - Copy",
+            "title": "RandomNumberGenerator - Copy"
+          },
+          "depth": 2,
+          "children": [
+            {
+              "type": "file",
+              "data": {
+                "language": {
+                  "nameMatchers": [".java"],
+                  "pygmentsLexer": "java",
+                  "multiLineComment": ["/*", "*", "*/"],
+                  "singleLineComment": ["//"],
+                  "ignorePrefix": "}",
+                  "foldPrefix": "^",
+                  "name": "Java"
+                },
+                "sourcePath": "C:\\Users\\adminprojitau\\Documents\\wikiINM\\code\\RandomNumberGenerator - Copy\\asssasasssasaaaaaa.java",
+                "projectPath": "code\\RandomNumberGenerator - Copy\\asssasasssasaaaaaa.java",
+                "targetPath": "code\\RandomNumberGenerator - Copy\\asssasasssasaaaaaa",
+                "pageTitle": "code\\RandomNumberGenerator - Copy\\asssasasssasaaaaaa",
+                "title": "asssasasssasaaaaaa"
+              },
+              "depth": 3,
+              "outline": []
+            }
+          ]
+        }, {
+          "type": "folder",
+          "data": {
             "path": "code/RandomNumberGenerator",
             "title": "RandomNumberGenerator"
           },
