0928f0bc335541f2f5d685cc0e4ad27123f8f0c9
bruno campos barone <brunocbarone1@yahoo.com.br>
2018-04-16 10:30:20 -0300

    inserindo HTMLs

--- doc/assets/behavior.js
+++ doc/assets/behavior.js
@@ -22,8 +22,7 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
         "pageTitle": "index",
         "title": "index"
       },
-      "depth": 1,
-      "outline": []
+      "depth": 1
     }, {
       "type": "folder",
       "data": {
